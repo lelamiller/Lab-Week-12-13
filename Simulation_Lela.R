8cc3fb7acc5f852596a2dfa8b015aa2bd9a1dace
lelamiller <lela_miller@brown.edu>
2025-11-19 14:40:01 -0500

    Update Simulation_Lela.R

--- Simulation_Lela.R
+++ Simulation_Lela.R
@@ -1,1 +1,15 @@
-citibike2 <- read.csv("/Users/lelamiller/Downloads/sample_bike.csv")
+#' @description simulates an example day
+#' @param data data frame with the start station, end station, hour and average rate of arrival for each trip across all days. 
+#' @return data frame of a simulated day: origin, destination, hour
+#
+library(tidyverse)
+arrival_rates_result <- read.csv("/Users/lelamiller/arrival_rates_result.csv")
+#function to simulate a day, return a data frame
+
+#Find lambda max- the maximum arrival rate in an hour in a given day
+lambda_max <- arrival_rates_result %>%
+  group_by(start_station, end_station) %>%
+  mutate(lambda_max = max(x_hat)) %>%
+  mutate(ei = dexp(rate = lambda_max)) %>%
+  mutate(ti = sum(ei))
+
