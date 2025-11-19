d0b396a56c28c2083df563bcd24896b665c58e0b
lelamiller <lela_miller@brown.edu>
2025-11-19 13:24:47 -0500

    Update arrival_rates.R

--- arrival_rates.R
+++ arrival_rates.R
@@ -1,71 +1,21 @@
 #' @description calculates the average rate of arrival for bikes
-#' @param data dataframe with columns X, start_station, end_station, start_time, end_time 
+#' @param citibike dataframe with columns X, start_station, end_station, start_time, end_time 
 #' @return data frame with the start station, end station, hour and average rate of arrival for each trip across all days. 
 #
 library(tidyverse)
-data <- read.csv("/Users/lelamiller/Downloads/sample_bike.csv")
 
-arrival_rates <- function(data) {
-  
-  # compute the average number of trips per hour between each pair
-  x_hat <- data %>%
+arrival_rates <- function(citibike){
+  citibike <- citibike %>%
     mutate(hour = hour(start_time)) %>%
-    filter(start_station != "R", end_station != "R") %>%
+    mutate(day = date(start_time)) %>%
+    filter(customer_type != "None") %>%
     group_by(start_station, end_station, hour) %>%
-    summarise(avg_trips = n() / n_distinct(as_date(start_time)), 
-              .groups = "drop") 
+    summarize(n = n()/n_distinct(day)) 
   
-  # pivot longer to get change in count 
-  data$end_station <- as.character(data$end_station)
-  trips_long <- data %>%
-    pivot_longer(cols = c("start_station", "start_time", 
-                          "end_station", "end_time"),
-                 names_to = c("type", ".value"),   
-                 names_pattern = "(start|end)_(.*)") %>%
-    mutate(change = ifelse(type == "start", -1, 1),
-           hour = hour(time)) %>%
-    select(station, time, hour, change)
-  
-  # add hour markers so we can get cumulative time
-  dates <- unique(as_date(trips_long$time))
-  hours <- c(seq(0,23,1),seq(0,23,1)+0.9999999)
-  stations <- unique(trips_long$station)
-  hr_pts <- expand.grid(time = dates, hour = hours, 
-                        station = stations) %>%
-    mutate(time = as.POSIXct(time) + hour*60*60,
-           hour = hour(time))
-  hr_pts$change <- 0
-  trips_long <- rbind(trips_long, hr_pts)
-  
-  # find average availability 
-  alpha_hat <- trips_long %>%
-    group_by(station) %>%
-    filter(station != "R") %>%
-    arrange(time) %>% 
-    mutate(count = cumsum(change),
-           date = as_date(time)) %>%
-    group_by(station, hour, date) %>%
-    summarize(time_avail = 
-                sum(difftime(time, lag(time), units="hours")*(count > 0), 
-                    na.rm = TRUE)) %>%
-    summarize(avg_avail = mean(time_avail)) %>%
-    mutate(avg_avail = round(as.numeric(avg_avail), digits = 4)) %>%
-    ungroup()
-  
-  # join the data and compute arrival rates
-  mu_hat <- x_hat %>%
-    left_join(alpha_hat, by = c("start_station" = "station", "hour")) %>%
-    mutate(mu_hat = ifelse(avg_avail > 0, avg_trips / avg_avail, NA))
-  
-  return(mu_hat)
+  return(citibike)
 }
 
-# Load the sample dataset
-bike_data <- read_csv("~/Desktop/PHP1560/Data/sample_bike.csv")
+citibike2 <- read.csv("/Users/lelamiller/Downloads/sample_bike.csv")
+citibike2 <- arrival_rates(citibike2)
 
-# Estimate arrival rates
-arrival_rates <- arrival_rates(bike_data)
-
-# View the results
-print(arrival_rates, n = 10)
 
