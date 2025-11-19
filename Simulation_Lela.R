5721ef575834ef733e7b6cd281121ce68f56a294
lelamiller <lela_miller@brown.edu>
2025-11-19 15:12:09 -0500

    Update Simulation_Lela.R

--- Simulation_Lela.R
+++ Simulation_Lela.R
@@ -7,9 +7,24 @@ arrival_rates_result <- read.csv("/Users/lelamiller/arrival_rates_result.csv")
 #function to simulate a day, return a data frame
 
 #Find lambda max- the maximum arrival rate in an hour in a given day
-lambda_max <- arrival_rates_result %>%
+lambda_max_df <- arrival_rates_result %>%
   group_by(start_station, end_station) %>%
-  mutate(lambda_max = max(x_hat)) %>%
-  mutate(ei = dexp(rate = lambda_max)) %>%
-  mutate(ti = sum(ei))
+  mutate(lambda_max = max(x_hat)) 
+
+lambda_max_vector <- as.vector(lambda_max_df$lambda_max)
+
+timeofday = 0 
+hour = 1
+arrivals <- c()
+arrivaln = 0
+while(timeofday < 24){
+  for(hour in 1:24){
+    if(timeofday < hour){
+      arrivaln <- arrivaln + 1
+      arrivals[hour] <- arrivaln
+      timeofday <- timeofday + rexp(lambda_max_vector)
+    }  
+    }
+}
+
 
