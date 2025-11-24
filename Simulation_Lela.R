9a911fbc1229a2eb51e68d5f6b760c2e5ad16a80
ciarameyer <ciara_meyer@brown.edu>
2025-11-24 14:08:19 -0500

    Update Simulation_Lela.R

--- Simulation_Lela.R
+++ Simulation_Lela.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 
 #read in the arrival rates data
+# arrival_rates_result <- read.csv("/Users/lelamiller/arrival_rates_result.csv")
 arrival_rates_result <- read.csv("~/Desktop/PHP1560/arrival_rates_result.csv")
 
 #START THE FUNCTION!!!
