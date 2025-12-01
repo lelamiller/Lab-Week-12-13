4980918b92fac9d6f73120f23c72745839c6d336
ciarameyer <ciara_meyer@brown.edu>
2025-11-30 21:10:02 -0500

    11/30/25 9:09 PM

--- results/results.R
+++ results/results.R
@@ -18,3 +18,37 @@ fleet_size_50 <- optimize_bike_placement(arrival_rates_result,
                                           seed = 123)
 
 write.csv(fleet_size_50, "fleet_size_50.csv", row.names = FALSE)
+
+library(tidyverse)
+library(ggplot2)
+
+one <- ggplot(fleet_size_200, aes(x = factor(station), y = recommended_bikes, 
+                           fill = recommended_bikes)) +
+  geom_col() +
+  theme_minimal() +
+  labs(title = "Optimized CitiBike Placement for a Fleet of 200 Bikes",
+       x = "Station Number",
+       y = "Recommended Number of Bikes", 
+       fill = "Recommended Number of Bikes") 
+
+two <- ggplot(fleet_size_100, aes(x = factor(station), y = recommended_bikes, 
+                           fill = recommended_bikes)) +
+  geom_col() +
+  theme_minimal() +
+  labs(title = "Optimized CitiBike Placement for a Fleet of 100 Bikes",
+       x = "Station Number",
+       y = "Recommended Number of Bikes", 
+       fill = "Recommended Number of Bikes") 
+
+three <- ggplot(fleet_size_50, aes(x = factor(station), y = recommended_bikes, 
+                           fill = recommended_bikes)) +
+  geom_col() +
+  theme_minimal() +
+  labs(title = "Optimized CitiBike Placement for a Fleet of 50 Bikes",
+       x = "Station Number",
+       y = "Recommended Number of Bikes", 
+       fill = "Recommended Number of Bikes") 
+
+ggsave("fleet_size_200.png", plot = one, width = 6, height = 4, dpi = 300)
+ggsave("fleet_size_100.png", plot = two, width = 6, height = 4, dpi = 300)
+ggsave("fleet_size_50.png", plot = three, width = 6, height = 4, dpi = 300)  
